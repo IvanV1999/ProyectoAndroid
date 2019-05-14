a67d1f0ad40e1c030cb73514e9969fb074c11a27
Ivan Velazquez <ivandvelazquez99@gmail.com>
2019-05-14 19:59:20 -0300

    Proyecto Base

---
+++ app/src/main/java/com/example/ivanvelazquez/proyectointent/ActBotoneada.java
@@ -0,0 +1,13 @@
+package com.example.ivanvelazquez.proyectointent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActBotoneada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_botoneada);
+    }
+}
---
+++ app/src/main/java/com/example/ivanvelazquez/proyectointent/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ivanvelazquez.proyectointent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void apretarBoton(View view){
+        String mensaje = "mensaje test";
+
+    }
+}
