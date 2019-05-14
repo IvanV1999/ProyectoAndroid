e44acf93c5053cd6795dbfb970b70832c30c5886
Ivan Velazquez <ivandvelazquez99@gmail.com>
2019-05-14 20:21:06 -0300

    Actualizacion de codigo, en un nuevo branch

--- app/src/main/java/com/example/ivanvelazquez/proyectointent/ActBotoneada.java
+++ app/src/main/java/com/example/ivanvelazquez/proyectointent/ActBotoneada.java
@@ -2,6 +2,7 @@ package com.example.ivanvelazquez.proyectointent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ActBotoneada extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class ActBotoneada extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_botoneada);
-
+        TVM = (TextView)findViewById(R.id.tvMensaje);
+        String mensajeEntrada = getIntent().getStringExtra("mensajeEntrada");
+        TVM.setText(mensajeEntrada);
+        Log.i("LOG DE INFO---","Se logueo el mensaje de entrada: "+mensajeEntrada);
     }
 }
--- app/src/main/java/com/example/ivanvelazquez/proyectointent/MainActivity.java
+++ app/src/main/java/com/example/ivanvelazquez/proyectointent/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ivanvelazquez.proyectointent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         String mensaje = "mensaje test";
         Intent actboton = new Intent(this, ActBotoneada.class);
         actboton.putExtra("mensajeEntrada",mensaje);
+        Log.i("LOG DE INFO---", "Se ingreso al metodo apretar boton y se enviara el mensaje" +mensaje);
         startActivity(actboton);
     }
 }
