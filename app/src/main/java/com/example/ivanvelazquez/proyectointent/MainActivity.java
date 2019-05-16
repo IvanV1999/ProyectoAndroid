95fe7dd712f8fbeaf2245062d9241874199924e0
Ivan Velazquez <ivandvelazquez99@gmail.com>
2019-05-16 11:44:54 -0300

    Se internacionalizó el hola mundo

--- app/src/main/java/com/example/ivanvelazquez/proyectointent/MainActivity.java
+++ app/src/main/java/com/example/ivanvelazquez/proyectointent/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.ivanvelazquez.proyectointent;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.TextView;
+
+        import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    String idioma = Locale.getDefault().toString();
+    private TextView holaMundo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        internacionalizar();
+    }
+
+    public void internacionalizar(){
+         holaMundo=(TextView)findViewById(R.id.tvHolaMundo);
+        if (idioma.equals("en_US")){
+            holaMundo.setText("Hello World");
+        }
+        else{
+            holaMundo.setText("Hola Mundo");
+        }
     }
 
     public void apretarBoton(View view){
