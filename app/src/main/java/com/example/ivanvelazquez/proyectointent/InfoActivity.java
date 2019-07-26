4ac567788df7f309294bd864cbc89c90d3c10a69
Ivan Velazquez <ivan.velazquez@globallogic.com>
2019-07-26 14:19:22 -0300

    Merge branch 'IntentimplicitoBranch' of https://github.com/IvanV1999/ProyectoAndroid into IntentimplicitoBranch
    # Conflicts:
    #	.idea/caches/gradle_models.ser
    #	app/src/main/java/Adapter/AdapterDatos.java
    #	app/src/main/java/com/example/ivanvelazquez/proyectointent/PostLoginActivity.java

--- app/src/main/java/com/example/ivanvelazquez/proyectointent/InfoActivity.java
+++ app/src/main/java/com/example/ivanvelazquez/proyectointent/InfoActivity.java
@@ -262,10 +262,7 @@ public class InfoActivity extends ButterBind implements FavoritoView.Callback {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                         takePic();
-                } else {
-
                 }
-                return;
             }
 
         }
